5ee36614e452530dd8b98906e28a429e6149d978
smittal-sumologic <127371380+smittal-sumologic@users.noreply.github.com>
2024-08-03 23:55:46 +0530

    Update boxplot.js

--- boxplot.js
+++ boxplot.js
@@ -8,8 +8,18 @@ const my_custom_viz = {
     },
 
     updateAsync: function(data, element, config, queryResponse, details, done) {
-        // ... code to modify chart ...
-        done();
+    var data = [
+        {
+            y: [1, 14, 14, 15, 16, 18, 18, 19, 19, 20, 20, 23],   // add the actual data
+            type: 'box'
+        }
+    ];
+
+    var layout = {
+        title: 'Box Plot'
+    };
+
+    Plotly.newPlot('myDiv', data, layout);   // Replace 'myDiv' with the id of your DOM element
     }
 }
 
