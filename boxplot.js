4766256f95268d570efe0020861c82e5607c0b41
smittal-sumologic <127371380+smittal-sumologic@users.noreply.github.com>
2024-08-03 23:53:25 +0530

    Update boxplot.js

--- boxplot.js
+++ boxplot.js
@@ -1,14 +1,53 @@
-updateAsync: function(data, element, config, queryResponse, details, done) {
-    var data = [
-        {
-            y: [1, 14, 14, 15, 16, 18, 18, 19, 19, 20, 20, 23],   // add the actual data
-            type: 'box'
-        }
-    ];
+import embed from "vega-embed";
+import { scatterHist } from "./scatter_hist";
+import { simpleHist } from "./simple_hist";
+import { baseOptions } from "./common/options";
+import { handleErrors } from "./common/utils/data";
+import "./common/styles.css";
 
-    var layout = {
-        title: 'Box Plot'
-    };
+looker.plugins.visualizations.add({
+  options: baseOptions,
+  create: function (element, config) {
+    var container = element.appendChild(document.createElement("div"));
+    container.setAttribute("id", "my-vega");
+  },
 
-    Plotly.newPlot('myDiv', data, layout);   // Replace 'myDiv' with the id of your DOM element
-}
+  updateAsync: function (data, element, config, queryResponse, details, done) {
+    if (data.length === 0) {
+      this.addError({ title: "No Results" });
+      done();
+      return;
+    }
+
+    if (config.bin_style === "binned_hist") {
+      if (
+        !handleErrors(this, queryResponse, {
+          min_pivots: 0,
+          max_pivots: 0,
+          min_dimensions: 1,
+          max_dimensions: undefined,
+          min_measures: 2,
+          max_measures: undefined,
+        })
+      )
+        return;
+
+      scatterHist(data, element, config, queryResponse, details, done, this, embed);
+    } else {
+      if (
+        !handleErrors(this, queryResponse, {
+          min_pivots: 0,
+          max_pivots: 0,
+          min_dimensions: 1,
+          max_dimensions: undefined,
+          min_measures: 1,
+          max_measures: undefined,
+        })
+      )
+        return;
+
+      simpleHist(data, element, config, queryResponse, details, done, this, embed);
+    }
+    done();
+  },
+});
